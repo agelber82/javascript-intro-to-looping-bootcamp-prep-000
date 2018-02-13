908a7e47f18e1ef97589a6fef462b65b593f8567
alandgelber@gmail.com <alandgelber@gmail.com>
2018-02-13 19:13:18 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,7 @@
-for (let i = 0; i < 25; i++) {
-}
-  if (i === 1) {
-  console.log("I am 1 strange loop");
-} else (i != 1); {
-  return("I am ${i} strange loops.");
-}
+   for ( let i = 0; i < 25; i++) {
++     if (i === 1) {
++       array.push(`I am 1 strange loop.`)
++     } else {
++       array.push(`I am ${i} strange loops.`)
++     }
++  }
